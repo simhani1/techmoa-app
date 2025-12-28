d204f1193e2ef08e3c9ff14b06afb7fd7b98ba12
simhani1 <simhani1@gmail.com>
2025-12-29 01:44:42 +0900

    feat: FixtureMonkey 의존성을 추가한다

--- app-storage/app-storage-db/build.gradle.kts
+++ app-storage/app-storage-db/build.gradle.kts
@@ -9,4 +9,8 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.1.15")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.assertj:assertj-core")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
