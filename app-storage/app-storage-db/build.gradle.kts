b5f69432421ceedc25d1d35f6c00196e178f5173
simhani1 <simhani1@gmail.com>
2025-12-29 03:34:17 +0900

    test: blog 엔티티를 테스트한다

--- app-storage/app-storage-db/build.gradle.kts
+++ app-storage/app-storage-db/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.1.15")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.assertj:assertj-core")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ app-storage/app-storage-db/src/test/kotlin/site/techmoa/app/storage/db/entity/BlogTest.kt
@@ -0,0 +1,48 @@
+package site.techmoa.app.storage.db.entity
+
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+import com.navercorp.fixturemonkey.kotlin.instantiator.instantiateBy
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class BlogTest : DescribeSpec({
+
+    describe("Blog의") {
+        val fixtureMonkey = FixtureMonkey.builder()
+            .plugin(KotlinPlugin())
+            .build()
+
+        val blog: Blog = fixtureMonkey.giveMeBuilder<Blog>()
+            .instantiateBy {
+                factory<Blog>("of") {
+                    parameter<String>("link")
+                    parameter<String>("logoUrl")
+                    parameter<String>("rssLink")
+                }
+            }
+            .set("link", "http://link.com")
+            .set("logoUrl", "http://logoUrl.com")
+            .set("rssLink", "http://rssLink.com")
+            .sample()
+        context("of()를 호출하면") {
+            it("ACTIVE 상태의 Blog를 생성한다.") {
+                blog.status.shouldBe(OperationStatus.ACTIVE)
+            }
+        }
+        context("delete()를 호출하면") {
+            it("DELETED 상태로 변경한다.") {
+                blog.delete()
+                blog.status.shouldBe(OperationStatus.DELETED)
+            }
+        }
+        context("pause()를 호출하면") {
+            it("PAUSED 상태로 변경한다.") {
+                blog.pause()
+                blog.status.shouldBe(OperationStatus.PAUSED)
+            }
+        }
+    }
+}){
+}
