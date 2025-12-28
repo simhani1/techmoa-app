a2241b31074bfeb326953e6b51ac367418164c68
simhani1 <simhani1@gmail.com>
2025-12-29 00:45:25 +0900

    feat: article 엔티티를 추가한다

---
+++ app-storage/app-storage-db/src/main/kotlin/site/techmoa/app/storage/db/entity/Article.kt
@@ -0,0 +1,47 @@
+package site.techmoa.app.storage.db.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "article")
+class Article(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id", nullable = false)
+    var id: Long? = null,
+
+    @Column(name = "blog_id", nullable = false)
+    val blogId: Long,
+
+    @Column(name = "title", nullable = false, length = 500)
+    val title: String,
+
+    @Column(name = "link", nullable = false, length = 1000)
+    val link: String,
+
+    @Column(name = "guid", nullable = false, length = 1000)
+    val guid: String,
+
+    @Column(name = "pub_date", nullable = false)
+    val pubDate: LocalDateTime,
+
+    @Column(name = "views", nullable = false)
+    private var views: Int = 0,
+) : BaseEntity() {
+
+    companion object {
+        fun of(
+            blogId: Long,
+            title: String,
+            link: String,
+            guid: String,
+            pubDate: LocalDateTime,
+        ): Article {
+            return Article(null, blogId, title, link, guid, pubDate)
+        }
+    }
+
+    fun increaseViews(amount: Int = 1) {
+        this.views += amount
+    }
+}
