c18444d33b57a40f87d0f26a8a02dd8a93791826
simhani1 <simhani1@gmail.com>
2025-12-31 00:39:25 +0900

    feat: api 모듈에 kotest 의존성을 추가한다

--- app-api/build.gradle.kts
+++ app-api/build.gradle.kts
@@ -10,7 +10,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.1.15")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testRuntimeOnly("com.h2database:h2")
 }
 
