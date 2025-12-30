85d278a98ad3ae93a42d9bf3a94d34e01588ad8f
simhani1 <simhani1@gmail.com>
2025-12-30 18:07:53 +0900

    feat: 게시글 조회에 필요한 쿼리를 구현한다

--- app-storage/app-storage-db/src/main/kotlin/site/techmoa/app/storage/db/repository/ArticleRepository.kt
+++ app-storage/app-storage-db/src/main/kotlin/site/techmoa/app/storage/db/repository/ArticleRepository.kt
@@ -1,8 +1,14 @@
 package site.techmoa.app.storage.db.repository
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import site.techmoa.app.storage.db.entity.Article
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import site.techmoa.app.storage.db.entity.ArticleEntity
 
-interface ArticleRepository : JpaRepository<Article, Long> {
+interface ArticleRepository : JpaRepository<ArticleEntity, Long> {
     fun existsByBlogIdAndGuid(blogId: Long, guid: String): Boolean
+
+    @Query(" select a from ArticleEntity a where (:cursor is null or a.pubDate < :cursor) order by a.pubDate desc")
+    fun findPublishedAfter(@Param("cursor") cursor: Long?, toPageable: Pageable): List<ArticleEntity>
 }
--- app-storage/app-storage-db/src/main/kotlin/site/techmoa/app/storage/db/repository/BlogRepository.kt
+++ app-storage/app-storage-db/src/main/kotlin/site/techmoa/app/storage/db/repository/BlogRepository.kt
@@ -3,11 +3,11 @@ package site.techmoa.app.storage.db.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import site.techmoa.app.storage.db.entity.Blog
+import site.techmoa.app.storage.db.entity.BlogEntity
 import site.techmoa.app.storage.db.entity.BlogStatus
 
-interface BlogRepository : JpaRepository<Blog, Long> {
+interface BlogRepository : JpaRepository<BlogEntity, Long> {
 
-    @Query("select b from Blog b where b._status = :status")
-    fun findAllStatus(@Param("status") status: BlogStatus): List<Blog>
+    @Query("select b from BlogEntity b where b._status = :status")
+    fun findAllStatus(@Param("status") status: BlogStatus): List<BlogEntity>
 }
