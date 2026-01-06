42d3f23060af183d62f0d0b137a2b4a7b1779419
simhani1 <simhani1@gmail.com>
2026-01-07 03:26:40 +0900

    fix: 테스트 주석 처리한다

--- app-api/src/test/kotlin/site/techmoa/app/fixture/article/ArticleFixture.kt
+++ app-api/src/test/kotlin/site/techmoa/app/fixture/article/ArticleFixture.kt
@@ -1,33 +1,33 @@
-package site.techmoa.app.fixture.article
-
-import com.navercorp.fixturemonkey.FixtureMonkey
-import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
-import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
-import com.navercorp.fixturemonkey.kotlin.instantiator.instantiateBy
-import site.techmoa.app.article.domain.Article
-
-class ArticleFixture {
-
-    companion object {
-
-        fun giveMe(cnt: Int = 0, blogId: Long): List<Article> {
-            return List(cnt) { default(blogId) }
-        }
-
-        private fun default(blogId: Long): Article {
-            val fixtureMonkey = FixtureMonkey.builder().plugin(KotlinPlugin()).build()
-
-            val article = fixtureMonkey.giveMeBuilder<Article>()
-                .instantiateBy {
-                    constructor<Article> {
-                        parameter<Long>("id")
-                        parameter<Long>("blogId")
-                    }
-                }
-                .set("blogId", blogId)
-                .sample()
-
-            return article
-        }
-    }
-}
+//package site.techmoa.app.fixture.article
+//
+//import com.navercorp.fixturemonkey.FixtureMonkey
+//import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+//import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+//import com.navercorp.fixturemonkey.kotlin.instantiator.instantiateBy
+//import site.techmoa.app.article.domain.Article
+//
+//class ArticleFixture {
+//
+//    companion object {
+//
+//        fun giveMe(cnt: Int = 0, blogId: Long): List<Article> {
+//            return List(cnt) { default(blogId) }
+//        }
+//
+//        private fun default(blogId: Long): Article {
+//            val fixtureMonkey = FixtureMonkey.builder().plugin(KotlinPlugin()).build()
+//
+//            val article = fixtureMonkey.giveMeBuilder<Article>()
+//                .instantiateBy {
+//                    constructor<Article> {
+//                        parameter<Long>("id")
+//                        parameter<Long>("blogId")
+//                    }
+//                }
+//                .set("blogId", blogId)
+//                .sample()
+//
+//            return article
+//        }
+//    }
+//}
--- app-api/src/test/kotlin/site/techmoa/app/fixture/blog/BlogFixture.kt
+++ app-api/src/test/kotlin/site/techmoa/app/fixture/blog/BlogFixture.kt
@@ -1,33 +1,33 @@
-package site.techmoa.app.fixture.blog
-
-import com.navercorp.fixturemonkey.FixtureMonkey
-import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
-import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
-import com.navercorp.fixturemonkey.kotlin.instantiator.instantiateBy
-import site.techmoa.app.blog.domain.Blog
-
-class BlogFixture {
-
-    companion object {
-
-        fun giveMe(cnt: Int = 0): List<Blog> {
-            return List(cnt) { index -> default((index + 1).toLong()) }
-        }
-
-        private fun default(blogId: Long): Blog {
-            val fixtureMonkey = FixtureMonkey.builder().plugin(KotlinPlugin()).build()
-
-            val blog = fixtureMonkey.giveMeBuilder<Blog>()
-                .instantiateBy {
-                    constructor<Blog> {
-                        parameter<Long>("id")
-                        parameter<String>()
-                    }
-                }
-                .set("id", blogId)
-                .sample()
-
-            return blog
-        }
-    }
-}
+//package site.techmoa.app.fixture.blog
+//
+//import com.navercorp.fixturemonkey.FixtureMonkey
+//import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+//import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+//import com.navercorp.fixturemonkey.kotlin.instantiator.instantiateBy
+//import site.techmoa.app.blog.domain.Blog
+//
+//class BlogFixture {
+//
+//    companion object {
+//
+//        fun giveMe(cnt: Int = 0): List<Blog> {
+//            return List(cnt) { index -> default((index + 1).toLong()) }
+//        }
+//
+//        private fun default(blogId: Long): Blog {
+//            val fixtureMonkey = FixtureMonkey.builder().plugin(KotlinPlugin()).build()
+//
+//            val blog = fixtureMonkey.giveMeBuilder<Blog>()
+//                .instantiateBy {
+//                    constructor<Blog> {
+//                        parameter<Long>("id")
+//                        parameter<String>()
+//                    }
+//                }
+//                .set("id", blogId)
+//                .sample()
+//
+//            return blog
+//        }
+//    }
+//}
