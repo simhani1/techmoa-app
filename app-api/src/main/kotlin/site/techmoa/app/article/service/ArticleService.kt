8d2f9fa13d1728982820d13b99fc509be45fa763
Jonghan Sim <simhani1@gmail.com>
2026-01-07 00:16:13 +0900

    refactor: article handler 클래스를 추가한다 (#46)
    refactor: handler 클래스를 추가한다

--- app-api/src/main/kotlin/site/techmoa/app/article/service/ArticleService.kt
+++ app-api/src/main/kotlin/site/techmoa/app/article/service/ArticleService.kt
@@ -1,22 +1,20 @@
 package site.techmoa.app.article.service
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import site.techmoa.app.article.domain.Article
 import site.techmoa.app.article.domain.ArticleContent
 import site.techmoa.app.article.support.ArticleFinder
+import site.techmoa.app.article.support.ArticleHandler
 import site.techmoa.app.blog.domain.Blog
 import site.techmoa.app.blog.support.BlogFinder
 import site.techmoa.app.core.OffsetLimit
 import site.techmoa.app.core.Page
-import site.techmoa.app.storage.db.repository.ArticleRepository
 
 @Service
 class ArticleService(
     private val articleFinder: ArticleFinder,
+    private val articleHandler: ArticleHandler,
     private val blogFinder: BlogFinder,
-    private val articleRepository: ArticleRepository
 ) {
     fun getArticles(cursor: Long?, limit: Int): Page<ArticleContent> {
         val fetchArticles = articleFinder.findPublishedAfter(cursor, OffsetLimit(limit = limit))
@@ -41,10 +39,7 @@ class ArticleService(
         return ArticleContent(article, blog)
     }
 
-    @Transactional
     fun increaseViewCount(articleId: Long) {
-        val article =
-            articleRepository.findByIdOrNull(articleId) ?: throw RuntimeException("Article with id $articleId not found")
-        article.increaseViews()
+        articleHandler.increaseViewCount(articleId)
     }
 }
