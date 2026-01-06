976c6adfdbc4b2f3c160691b385efa89f36240e9
simhani1 <simhani1@gmail.com>
2026-01-07 00:11:56 +0900

    refactor: handler 클래스를 추가한다

--- app-api/src/test/kotlin/site/techmoa/app/article/service/ArticleServiceUnitTest.kt
+++ app-api/src/test/kotlin/site/techmoa/app/article/service/ArticleServiceUnitTest.kt
@@ -4,8 +4,8 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import site.techmoa.app.ServiceUnitTest
 import site.techmoa.app.article.support.ArticleFinder
+import site.techmoa.app.article.support.ArticleHandler
 import site.techmoa.app.blog.support.BlogFinder
-import site.techmoa.app.storage.db.repository.ArticleRepository
 
 abstract class ArticleServiceUnitTest: ServiceUnitTest() {
     @InjectMockKs
@@ -15,8 +15,8 @@ abstract class ArticleServiceUnitTest: ServiceUnitTest() {
     lateinit var articleFinder: ArticleFinder
 
     @MockK
-    lateinit var blogFinder: BlogFinder
+    lateinit var articleHandler: ArticleHandler
 
     @MockK
-    lateinit var articleRepository: ArticleRepository
+    lateinit var blogFinder: BlogFinder
 }
