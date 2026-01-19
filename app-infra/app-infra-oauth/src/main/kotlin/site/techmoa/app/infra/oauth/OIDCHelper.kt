797342eabe27b69d0959c491745aa7164c187d08
Jonghan Sim <simhani1@gmail.com>
2026-01-20 04:33:35 +0900

    feat: 로그인 로그 출력을 추가한다 (#65)
    * feat: 로깅을 추가한다
    
    * fix: 헤더 파싱 로직을 수정한다

--- app-infra/app-infra-oauth/src/main/kotlin/site/techmoa/app/infra/oauth/OIDCHelper.kt
+++ app-infra/app-infra-oauth/src/main/kotlin/site/techmoa/app/infra/oauth/OIDCHelper.kt
@@ -1,5 +1,6 @@
 package site.techmoa.app.infra.oauth
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import site.techmoa.app.infra.oauth.dto.OIDCPayload
 import site.techmoa.app.infra.oauth.dto.OIDCPubKeyDto
@@ -11,11 +12,17 @@ class OIDCHelper(
     properties: OauthProperties
 ) {
 
+    private val log = LoggerFactory.getLogger(this.javaClass)
     private val property = properties.kakao
 
     fun getPayload(idToken: String, pubKeys: OIDCPubKeys): OIDCPayload {
+        log.info("[OIDC] Extracting kid from unsigned token - expected issuer: ${property.baseUrl}, expected audience: ${property.clientId}")
         val kid = getKidFromUnsignedToken(idToken, property.baseUrl, property.clientId)
+        log.info("[OIDC] Kid extracted: $kid")
+        log.info("[OIDC] Matching public key for kid: $kid")
         val pubKey = getPubKey(pubKeys, kid)
+        log.info("[OIDC] Public key matched - alg: ${pubKey.alg}, use: ${pubKey.use}")
+        log.info("[OIDC] Extracting payload with public key")
         return getPayload(idToken, pubKey)
     }
 
@@ -28,7 +35,7 @@ class OIDCHelper(
     private fun getPubKey(
         pubKeys: OIDCPubKeys,
         kid: String
-    ): OIDCPubKeyDto = pubKeys.keys.firstOrNull { it.kid == kid } ?: throw RuntimeException("No matched keys")
+    ): OIDCPubKeyDto = pubKeys.keys.firstOrNull { it.kid == kid } ?: throw RuntimeException("No matched keys for kid: $kid")
 
     private fun getPayload(
         idToken: String,
