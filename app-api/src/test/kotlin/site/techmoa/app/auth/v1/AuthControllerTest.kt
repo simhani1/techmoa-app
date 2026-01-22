bdcbd0fdb2cd9e6d185ecfde170e770aa6f555a6
simhani1 <simhani1@gmail.com>
2026-01-22 16:55:33 +0900

    fix: 헤더로 토큰을 반환한다

---
+++ app-api/src/test/kotlin/site/techmoa/app/auth/v1/AuthControllerTest.kt
@@ -0,0 +1,45 @@
+package site.techmoa.app.auth.v1
+
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import site.techmoa.app.core.auth.OauthLoginUseCase
+import site.techmoa.app.core.auth.dto.AuthToken
+
+@WebMvcTest(controllers = [AuthController::class])
+@AutoConfigureMockMvc(addFilters = false)
+class AuthControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockitoBean
+    lateinit var oauthLoginUseCase: OauthLoginUseCase
+
+    @Test
+    fun `카카오 로그인 후 Access Token을 헤더로 응답한다`() {
+        // GIVEN
+        val code = "kakao-code"
+        val accessToken = "access-token"
+
+        // WHEN
+        given(oauthLoginUseCase.process(code)).willReturn(AuthToken(accessToken))
+
+        val response = mockMvc.get("/v1/oauth/kakao/callback") {
+            param("code", code)
+        }
+
+        // THEN
+        response.andExpect {
+            status { isOk() }
+            header { string(HttpHeaders.AUTHORIZATION, "Bearer $accessToken") }
+        }
+    }
+
+}
