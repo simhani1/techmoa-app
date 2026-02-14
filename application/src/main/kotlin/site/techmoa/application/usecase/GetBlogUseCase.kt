76b7dec666f0e4c09a6049a96f8926ebf05203d8
simhani1 <simhani1@gmail.com>
2026-02-15 03:52:52 +0900

    refactor: move article paging and blog map lookup into query use cases

---
+++ application/src/main/kotlin/site/techmoa/application/usecase/GetBlogUseCase.kt
@@ -0,0 +1,25 @@
+package site.techmoa.application.usecase
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import site.techmoa.application.port.BlogPort
+import site.techmoa.domain.exception.NotFoundException
+import site.techmoa.domain.model.Blog
+
+@Component
+class GetBlogUseCase(
+    private val blogPort: BlogPort
+) {
+
+    @Transactional(readOnly = true)
+    fun fetchOne(blogId: Long): Blog {
+        val blog = blogPort.findById(blogId)
+            ?: throw NotFoundException("Blog not found with id: $blogId")
+        return blog
+    }
+
+    @Transactional(readOnly = true)
+    fun fetchBlogMapByIds(blogIds: List<Long>): Map<Long, Blog> {
+        return blogPort.findAllBy(blogIds).associateBy { it.id }
+    }
+}
