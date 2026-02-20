0352e86b9c9d6d3de028cc2d5cc42ac9ea1d9e64
simhani1 <simhani1@gmail.com>
2026-02-21 05:21:49 +0900

    fix: 유스케이스 트랜잭션을 제거한다

--- application/src/main/kotlin/site/techmoa/application/usecase/MemberUseCase.kt
+++ application/src/main/kotlin/site/techmoa/application/usecase/MemberUseCase.kt
@@ -1,7 +1,6 @@
 package site.techmoa.application.usecase
 
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 import site.techmoa.application.dto.MemberResource
 import site.techmoa.application.port.MemberPort
 import site.techmoa.domain.model.Member
@@ -12,7 +11,6 @@ class MemberUseCase(
     private val memberPort: MemberPort
 ) {
 
-    @Transactional
     fun getOrSave(resource: MemberResource): Member {
         return when (val member = memberPort.findByProviderAndSubject(resource.provider, resource.subject)) {
             is MemberLookupResult.NewMember -> memberPort.save(resource)
