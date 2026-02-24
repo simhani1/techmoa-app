facc258fbaa5116c574c8fef9b455914ee655cd9
simhani1 <simhani1@gmail.com>
2026-02-25 04:07:40 +0900

    feat: 블로그 목록 조회 응답 형식을 추가한다

--- presentation/src/main/kotlin/site/techmoa/presentation/controller/BlogControllerV1.kt
+++ presentation/src/main/kotlin/site/techmoa/presentation/controller/BlogControllerV1.kt
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import site.techmoa.application.service.BlogService
-import site.techmoa.domain.model.Blog
 import site.techmoa.presentation.common.template.ApiResponse
+import site.techmoa.presentation.controller.response.GetAllBlogsResponse
 
 @RequestMapping("/v1/blogs")
 @RestController
@@ -13,9 +13,17 @@ class BlogControllerV1(
     private val blogService: BlogService,
 ) {
 
-    @GetMapping()
-    fun getAllBlogs(): ApiResponse<List<Blog>> {
-        val blogs = blogService.getAllBlogs()
-        return ApiResponse.success(blogs)
+    @GetMapping
+    fun getAllBlogs(): ApiResponse<List<GetAllBlogsResponse>> {
+        val response = blogService.getAllBlogs()
+            .sortedBy { it.name }
+            .mapIndexed { idx, blog ->
+                GetAllBlogsResponse(
+                    order = idx + 1,
+                    name = blog.name,
+                    link = blog.link,
+                )
+            }
+        return ApiResponse.success(response)
     }
 }
