6f666f11a4295d362ce5cddf919a069977bf4063
simhani1 <simhani1@gmail.com>
2026-02-27 00:56:56 +0900

    fix: 아티클 리포지토리 미사용 조회 메서드를 제거한다

--- batch/schedules/src/main/kotlin/site/techmoa/batch/schedules/repository/ArticleRepository.kt
+++ batch/schedules/src/main/kotlin/site/techmoa/batch/schedules/repository/ArticleRepository.kt
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
 import site.techmoa.batch.schedules.dto.NewArticleDto
-import site.techmoa.domain.model.Article
 
 @Repository(value = "SchedulesArticleRepository")
 class ArticleRepository(
@@ -33,34 +32,5 @@ class ArticleRepository(
             },
             articleId
         )
-    }
-
-    fun findByBlogIdAndGuid(blogId: Long, guid: String): Article? {
-        val sql = """
-            SELECT article_id, blog_id, title, link, guid, pub_date, views
-            FROM article
-            WHERE blog_id = ?
-                AND guid = ?
-            LIMIT 1
-        """.trimIndent()
-
-        val result = jdbcTemplate.query(
-            sql,
-            RowMapper { rs, _ ->
-                Article(
-                    id = rs.getLong("article_id"),
-                    blogId = rs.getLong("blog_id"),
-                    title = rs.getString("title"),
-                    link = rs.getString("link"),
-                    guid = rs.getString("guid"),
-                    pubDate = rs.getLong("pub_date"),
-                    views = rs.getInt("views")
-                )
-            },
-            blogId,
-            guid
-        )
-
-        return result.firstOrNull()
     }
 }
